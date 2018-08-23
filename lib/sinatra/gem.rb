fa0a303411d206e3c144df49d079948d2d09c841
stbnrivas <stbnrivas@gmail.com>
2018-08-23 15:32:42 +0200

    initial commit  gem sinatra-gem

---
+++ lib/sinatra/gem.rb
@@ -0,0 +1,7 @@
+require "sinatra/gem/version"
+
+module Sinatra
+  module Gem
+    # Your code goes here...
+  end
+end
---
+++ spec/sinatra/gem_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Sinatra::Gem do
+  it "has a version number" do
+    expect(Sinatra::Gem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
